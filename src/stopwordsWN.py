718624d31607e6229c0bcfa393f7fb1d1d1110f5
David Abraham <daveynoire97@gmail.com>
2018-02-05 13:06:29 +0530

    Modify insults data and fix paths for notebooks

---
+++ src/stopwordsWN.py
@@ -0,0 +1,32 @@
+STOP_WORDS = set("""
+a about according across actually afterwards again against ago all almost along
+already also although always am among amongst an and any anyhow
+anyone anything anyway anywhere are aren't around as at
+back be became because become becomes becoming been before beforehand
+being besides beyond both but by
+d did didn't does doesn't due during
+each earlier either else elsewhere empty enough etc even ever every
+everyone everything everywhere except
+for former formerly found from full further
+he his himself had has have he hence her here hereafter hereby herein hereupon hers herself
+him himself his however hundred
+i if in inc instead into is it its itself 
+just
+latter latterly let like likely
+made maybe many me meanwhile might mine moreover most mostly mr mrs much my myself
+namely neither never nevertheless nowhere nor 
+of off often only onto or other others otherwise our ours ourselves out overall own
+part per perhaps please
+rather re recent recently really regarding
+s said say seem seemed seeming seems serious several shall she
+since so some somehow someone something sometime sometimes somewhere specially 
+still such
+t than that the their them themselves then thence there thereafter
+thereby therefore therein thereupon these they this those though
+through throughout thru thus together too top 
+unless unlike unlikely until up upon unless upon us used using
+ve various very via was we well were whatever whence whenever
+whereafter whereas whereby wherein whereupon wherever whether
+whither whoever whole whomever whose will with within without would
+yet you your yours yourself yourselves
+""".split())
